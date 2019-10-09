d55b94fb054d0d7c26e9ad9337713df315d22060
Rikk Hill <rikk@unashamedly.co.uk>
2019-10-09 11:49:00 +0100

    Added indexing on series

--- src/my_pandas/series.py
+++ src/my_pandas/series.py
@@ -10,3 +10,15 @@ class Series:
 
         # Eventually the index will be its own all-singing, all-dancing object
         self.index = list(range(1, len(collection) + 1))
+
+    def __getitem__(self, item):
+
+        try:
+            return self.values[self.index.index(item)]
+
+        except (KeyError, IndexError):
+            pass
+
+
+
+
--- tests/test_series.py
+++ tests/test_series.py
@@ -14,3 +14,24 @@ def test_series_builds_from_list():
     assert series.index == list(range(1, len(my_list) + 1)), "Series index is sequential integers"
     assert series.values == my_list, "Series values are what we put in"
 
+
+def test_get_series_element_from_subscript():
+
+    my_list = [9, 8, 7, 6, 5]
+
+    series = Series(my_list)
+
+    assert series[2] == 8
+
+
+def test_get_series_element_from_index():
+
+    my_list = [9, 8, 7, 6, 5]
+
+    series = Series(my_list)
+
+    subselect = series.loc[2]
+
+    assert isinstance(subselect, Series), "Subselection of series is also a series"
+
+    assert subselect.values == [7]
